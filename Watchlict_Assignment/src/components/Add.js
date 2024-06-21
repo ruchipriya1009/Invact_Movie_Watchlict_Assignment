4595ce58360a83fe79e75d155517632a79c6f49b
--ruchi <ruchipriya1009@gmail.com>
2024-06-21 19:03:18 +0530

    added api

--- Watchlict_Assignment/src/components/Add.js
+++ Watchlict_Assignment/src/components/Add.js
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ResultCard } from "./ResultCard";
 
 export const Add = () => {
   const [searchedMovie, setSearchedMovie] = useState("");
   const [results, setResults] = useState([]);
+  const [showAllMovies, setShowAllMovies] = useState(false);
 
-  const onChange = (e) => {
-    e.preventDefault();
-    setSearchedMovie(e.target.value);
+  useEffect(() => {
+    if (showAllMovies) {
+      fetchPopularMovies();
+    } else {
+      setResults([]);
+    }
+  }, [showAllMovies]);
 
+  const fetchPopularMovies = () => {
     fetch(
-      `
-      https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+      `https://api.themoviedb.org/3/movie/popular?api_key=a43d3e2b14ed47ed4026ffef42e90e66&language=en-US&page=1`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -23,10 +28,49 @@ export const Add = () => {
       });
   };
 
+  const toggleMovieList = () => {
+    setShowAllMovies(!showAllMovies);
+  };
+
+  const onChange = (e) => {
+    e.preventDefault();
+    setSearchedMovie(e.target.value);
+
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=a43d3e2b14ed47ed4026ffef42e90e66&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.errors) {
+          setResults(data.results);
+          setShowAllMovies(false); // Reset showAllMovies when searching
+        } else {
+          setResults([]);
+        }
+      });
+  };
+
   return (
     <div className="add-page">
       <div className="container">
         <div className="add-content">
+        <button
+        onClick={toggleMovieList}
+        style={{
+          padding: '10px 20px',  
+          backgroundColor: '#ff6347',  
+          color: 'white', 
+          border: 'none',  
+          cursor: 'pointer',
+          
+          marginLeft: '200px',
+          marginBottom: '30px'
+           
+        }}
+      >
+        {showAllMovies ? "Hide Movie List" : "Show All Movies"}
+      </button>
+      
           <div className="input-wrapper">
             <input
               type="text"
@@ -35,7 +79,7 @@ export const Add = () => {
               onChange={onChange}
             />
           </div>
-          {results.length > 0 && (
+          {(results.length > 0 || showAllMovies) && (
             <ul className="results">
               {results.map((movie) => (
                 <li key={movie.id}>
